37a598e3ae58eece930fb24389bbd084e88beabf
Karmanshi Shrivastava <80811897+karmanshi@users.noreply.github.com>
2024-08-30 11:09:04 +0000

    case-convertor

---
+++ case-convertor/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Caseconvertor from './Component/Caseconvertor';
+
+function App() {
+  return (
+    <>
+    <Caseconvertor/></>
+  );
+}
+
+export default App;
